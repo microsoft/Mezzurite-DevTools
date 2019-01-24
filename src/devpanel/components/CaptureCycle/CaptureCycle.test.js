fe2b0af1a62208ff54079252114fe871dbc111b4
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-01-23 17:20:49 -0800

    Add CaptureCycle component

---
+++ src/devpanel/components/CaptureCycle/CaptureCycle.test.js
@@ -0,0 +1,23 @@
+import CaptureCycle from './CaptureCycle';
+
+describe('CaptureCycle.test.js', () => {
+  it('should not render when the props are null', () => {
+    expect(CaptureCycle(null)).toBeFalsy();
+  });
+
+  it('should not render when the timings are null', () => {
+    expect(CaptureCycle({ timings: null })).toBeFalsy();
+  });
+
+  it('should not render when there are no timings', () => {
+    expect(CaptureCycle({ timings: [] })).toBeFalsy();
+  });
+
+  it('should not error out when there is no timestamp', () => {
+    expect(CaptureCycle({ captureCycleIndex: 0, timings: [ { componentLoadTime: 9.2, componentName: 'componentName' } ] })).toMatchSnapshot();
+  });
+
+  it('should not error out when there is no captureCycleIndex', () => {
+    expect(CaptureCycle({ timestamp: new Date(2019, 1, 23), timings: [ { componentLoadTime: 9.2, componentName: 'componentName' } ] })).toMatchSnapshot();
+  });
+});
