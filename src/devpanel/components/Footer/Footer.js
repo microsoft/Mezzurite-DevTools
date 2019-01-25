b1daec3b56defe7996367081fdb5b51dc508f58e
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-01-24 17:40:26 -0800

    Rework footer component

--- src/devpanel/components/Footer/Footer.js
+++ src/devpanel/components/Footer/Footer.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import { string } from 'prop-types';
+
 import './Footer.css';
+import GithubMark from './GithubMark';
 
-import githubLogo from './GitHub-Mark-Light-32px.png';
-
-const Footer = () => (
-  <footer className='Footer'>
-    <a href='https://github.com/Microsoft/Mezzurite-DevTools' target='_blank'>
-      <img id='gh-logo' src={githubLogo} />
-    </a>
-    <span>Visit us on GitHub! ~ The Mezzurite Development Team</span>
+const Footer = (props) => (
+  <footer role='contentinfo'>
+    <ul className='footer--content'>
+      <li className='github-information'>
+        <a
+          ariaLabel='Visit Mezzurite Dev Tools on GitHub'
+          className='github-information--link'
+          href='https://github.com/Microsoft/Mezzurite-DevTools'
+          target='_blank'
+        >
+          <GithubMark />
+          <span className='github-information--text'>Visit us on GitHub!<span className='tablet-hidden'> ~ The Mezzurite Development Team</span></span>
+        </a>
+      </li>
+      <li className='package-information'>
+        <ul className='package-information--container'>
+          {props.packageName != null && <li className='package-information--name'>
+            <span className='package-information--label'><span className='tablet-hidden'>Package </span>Name:</span>
+            <a ariaLabel='Visit Mezzurite on GitHub' className='github-information--package' href='https://github.com/Microsoft/Mezzurite' target='_blank'>
+              {props.packageName}
+            </a>
+          </li>}
+          {props.packageVersion != null && <li className='package-information--version'>
+            <span className='package-information--label'><span className='tablet-hidden'>Package </span>Version:</span>{props.packageVersion}
+          </li>}
+        </ul>
+      </li>
+    </ul>
   </footer>
 );
+
+Footer.propTypes = {
+  packageName: string,
+  packageVersion: string
+};
 
 export default Footer;
