eaf1bc30b32232095b98a22039aea7ea5224a20a
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-01-25 10:33:38 -0800

    Refactor long inline conditional in Main.js

--- src/devpanel/components/Main/Main.js
+++ src/devpanel/components/Main/Main.js
@@ -4,25 +4,27 @@ import { arrayOf, number, shape, string } from 'prop-types';
 import './Main.css';
 import CaptureCycle from '../CaptureCycle/CaptureCycle';
 
-const Main = (props) => (
-  props != null && <main className='main'>
-    {props.applicationLoadTime == null && props.captureCycles == null &&
-      <h2 className='main--header'>Mezzurite is working...</h2>
-    }
-    {props.applicationLoadTime != null && <h2 className='main--header'>App<span className='mobile-hidden'>lication</span> Load Time: {props.applicationLoadTime} ms</h2>}
-    {props.captureCycles != null && props.captureCycles.length > 0 && props.captureCycles.filter((captureCycle) => captureCycle != null).map((captureCycle, captureCycleIndex) =>
-      <CaptureCycle
-        captureCycleIndex={captureCycleIndex}
-        key={`capture-cycle-${captureCycleIndex}`}
-        timestamp={captureCycle.time}
-        timings={captureCycle.componentTimings}
-      />
-    )}
-    {props.captureCycles != null && (props.captureCycles == null || props.captureCycles.length === 0) && props.applicationLoadTime != null &&
-      <h3>No active capture cycles have completed.</h3>
-    }
-  </main>
-);
+const Main = (props) => {
+  const showCaptureCycles = props.captureCycles != null && (props.captureCycles == null || props.captureCycles.length === 0) && props.applicationLoadTime != null;
+
+  return (
+    props != null && <main className='main'>
+      {props.applicationLoadTime == null && props.captureCycles == null &&
+        <h2 className='main--header'>Mezzurite is working...</h2>
+      }
+      {props.applicationLoadTime != null && <h2 className='main--header'>App<span className='mobile-hidden'>lication</span> Load Time: {props.applicationLoadTime} ms</h2>}
+      {props.captureCycles != null && props.captureCycles.length > 0 && props.captureCycles.filter((captureCycle) => captureCycle != null).map((captureCycle, captureCycleIndex) =>
+        <CaptureCycle
+          captureCycleIndex={captureCycleIndex}
+          key={`capture-cycle-${captureCycleIndex}`}
+          timestamp={captureCycle.time}
+          timings={captureCycle.componentTimings}
+        />
+      )}
+      {showCaptureCycles && <h3>No active capture cycles have completed.</h3>}
+    </main>
+  );
+};
 
 Main.propTypes = {
   applicationLoadTime: number,
